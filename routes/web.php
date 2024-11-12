53db59578436316d516f2d6a5f74a2b24ac3065f
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2024-11-12 11:52:03 +0200

    Redirect to login page after logout and on initial server access

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
+// Перенаправлення кореневої сторінки на логін
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 // Перенаправлення на компанії з dashboard
 Route::get('/dashboard', function () {
     return redirect()->route('companies.index');
@@ -10,20 +15,14 @@ Route::get('/dashboard', function () {
 
 // Група маршрутів для компаній з використанням middleware
 Route::middleware(['auth'])->group(function () {
-    // Index route for companies
     Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
-
-    // Route to show the form for creating a new company
     Route::get('/companies/create', [CompanyController::class, 'create'])->name('companies.create');
-
-    // Route to store a new company
     Route::post('/companies', [CompanyController::class, 'store'])->name('companies.store');
-
-    // Other routes for editing, updating, and deleting companies
     Route::get('/companies/{id}/edit', [CompanyController::class, 'edit'])->name('companies.edit');
     Route::put('/companies/{id}', [CompanyController::class, 'update'])->name('companies.update');
     Route::delete('/companies/{id}', [CompanyController::class, 'destroy'])->name('companies.destroy');
 });
 
-// Додайте маршрути для профілю, якщо потрібно
+// Додайте маршрути для аутентифікації
 require __DIR__.'/auth.php';
+
